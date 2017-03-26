9236d5b244ae8ec04ae6532fdba6b114066e26a9
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-03-26 00:55:43 -0300

    Mejoras en unit tests de Usuarios.

--- tests/AppBundle/Controller/UsersControllerTest.php
+++ tests/AppBundle/Controller/UsersControllerTest.php
@@ -6,15 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UsersControllerTest extends WebTestCase
 {
+    public function testGetVersion()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/version');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Version', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
+    public function testGetCountUsers()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/users/count');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Cantidad', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
     public function testGetUsers()
     {
         $client = static::createClient();
         $client->request('GET', '/users');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue(
-            $client->getResponse()->headers->contains('Content-Type', 'application/json')
-        );
+        $this->assertContains('id', $client->getResponse()->getContent());
+        $this->assertContains('name', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
     }
 
     public function testGetUser()
@@ -23,33 +49,90 @@ class UsersControllerTest extends WebTestCase
         $client->request('GET', '/users/1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue(
-            $client->getResponse()->headers->contains('Content-Type', 'application/json')
-        );
+        $this->assertContains('id', $client->getResponse()->getContent());
+        $this->assertContains('name', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
     }
 
-    public function testNewUser()
+    public function testGetUserNotFound()
     {
         $client = static::createClient();
-        $client->request('POST', '/users/', array('name' => 'Matías', 'email' => 'mati@hotmail.com'));
+        $client->request('GET', '/users/123');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertNotContains('id', $client->getResponse()->getContent());
+        $this->assertNotContains('name', $client->getResponse()->getContent());
+        $this->assertContains('no existe', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
+    public function testCreateUser()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/users/', array('name' => 'Matías'));
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('correctamente', $client->getResponse()->getContent());
-        $this->assertTrue(
-            $client->getResponse()->headers->contains('Content-Type', 'application/json')
-        );
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
+    public function testCreateUserWithoutName()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/users/', array());
+
+        $this->assertEquals(406, $client->getResponse()->getStatusCode());
+        $this->assertNotContains('correctamente', $client->getResponse()->getContent());
+        $this->assertContains('Ingrese', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
     }
 
     public function testUpdateUser()
     {
         $client = static::createClient();
-        $client->request('PUT', '/users/1', array('name' => 'Mago', 'email' => 'marquitos@gmail.com'));
+        $client->request(
+            'PUT', '/users/1', array('name' => 'Mago', 'email' => 'mago@hotmail.com')
+        );
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('correctamente', $client->getResponse()->getContent());
-        $this->assertTrue(
-            $client->getResponse()->headers->contains('Content-Type', 'application/json')
-        );
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
+    public function testUpdateUserWithoutName()
+    {
+        $client = static::createClient();
+        $client->request('PUT', '/users/1', array());
+
+        $this->assertEquals(406, $client->getResponse()->getStatusCode());
+        $this->assertNotContains('correctamente', $client->getResponse()->getContent());
+        $this->assertContains('Ingrese', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
+    public function testUpdateUserNotFound()
+    {
+        $client = static::createClient();
+        $client->request('PUT', '/users/123456789', array());
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertNotContains('correctamente', $client->getResponse()->getContent());
+        $this->assertContains('no existe', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
     }
 
     public function testDeleteUser()
@@ -59,8 +142,21 @@ class UsersControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('correctamente', $client->getResponse()->getContent());
-        $this->assertTrue(
-            $client->getResponse()->headers->contains('Content-Type', 'application/json')
-        );
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
+    }
+
+    public function testDeleteUserNotFound()
+    {
+        $client = static::createClient();
+        $client->request('DELETE', '/users/123456789');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertNotContains('correctamente', $client->getResponse()->getContent());
+        $this->assertContains('no existe', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->headers->contains(
+            'Content-Type', 'application/json'
+        ));
     }
 }
